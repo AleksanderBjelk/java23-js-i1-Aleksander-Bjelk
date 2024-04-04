2bb8df388effcfabb1e6db848f9a2abe446578dc
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-04-04 14:29:34 +0200

    tänker om i html pga behöver skilja åt spelare

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//alla variabler som kommer användas
+const player1El = document.querySelector(".player_1");
+const player2El = document.querySelector(".player_2");
+const currentPointsP1El = document.getElementById("currentPoints_P1");
+const currentPointsP2El = document.getElementById("currentPoints_P2");
+const totalPointsP1El = document.getElementById('tPoints_P1');
+const totalPointsP2El = document.getElementById('tPoints_P2');
+
